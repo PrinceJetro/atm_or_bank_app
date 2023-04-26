65460eebc1b5e23071ac7d83c1ce0590bf7e0b1c
Adegbuyi Jephthah <114796977+PrinceJetro@users.noreply.github.com>
2023-04-26 15:52:17 +0100

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    message = "Example jetro project on vercel"
+    message = "Deployed"
     return HttpResponse(message)
